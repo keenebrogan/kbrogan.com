c5000d34f4a2ee64c84dd30006bb45bda2f5594c
Keene Brogan <kbrogan@gatech.edu>
2025-02-01 17:56:51 -0500

    changed nav bar customization

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Display an alert when the user clicks the button
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".btn-primary");
+    button.addEventListener("click", function() {
+        alert("Welcome to my projects page!");
+    });
+});
