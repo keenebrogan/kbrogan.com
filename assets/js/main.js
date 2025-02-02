aeb9970afac93e0ce7e7324ebe1833e7db9d39da
Keene Brogan <kbrogan@gatech.edu>
2025-02-01 20:03:37 -0500

    first draft

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 // Display an alert when the user clicks the button
 document.addEventListener("DOMContentLoaded", function() {
     const button = document.querySelector(".btn-primary");
-    button.addEventListener("click", function() {
-        alert("Welcome to my projects page!");
-    });
+    // button.addEventListener("click", function() {
+    //     alert("Welcome to my projects page!");
+    // });
 });
